202834d06938706379eb7b4fa8b77fe49545a560
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-03-25 11:49:16 +1300

    Made base for submit project page. This includes all static aspects

--- src/pages/StudentDashboard/components/SubmitProject/SubmitProject.jsx
+++ src/pages/StudentDashboard/components/SubmitProject/SubmitProject.jsx
@@ -1,7 +1,33 @@
+import styles from "./SubmitProject.module.css";
+import projectImg from "../../../../assets/StudentDashboard/makeProject-screenshot.png";
+import submitPojectImg from "../../../../assets/StudentDashboard/submitProject-Photo.png";
+
 export default function SubmitProject() {
   return (
-    <div>
-      <p>SubmitPoject</p>
+    <div className={styles.flexMain}>
+      <div className={styles.content}>
+        <div className={styles.flex} id={styles.left}>
+          <img src={projectImg} alt="Project image" className={styles.img} />
+          <h3>Submit project photo</h3>
+          <p className={styles.text}>
+            After completing your project, take a screenshot of your project and
+            upload it here.
+          </p>
+        </div>
+        <div className={styles.flex}>
+          <img
+            src={submitPojectImg}
+            alt="submit project image"
+            className={styles.img}
+          />
+
+          <h3>Show your teacher</h3>
+          <p className={styles.text}>
+            If your teacher is in the same room as you. Click the button to let
+            them know you are done.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
