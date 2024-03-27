7809d447bb45aa52fb9fb06ad9c4f7d77f0e6a1b
cyrus_w <cyrusw89@gmail.com>
2024-03-28 12:32:46 +1300

    css updates for progress tracker and student profiles

--- src/common/Dashboard/Dashboard.jsx
+++ src/common/Dashboard/Dashboard.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import styles from './Dashboard.module.css';
-//src assets
+//src assets for toggle + bottom nav
 import arrowLeft from '../../assets/arrowLeft.png';
 import profile from '../../assets/profile.png';
 import settings from '../../assets/settings.png';
 import logout from '../../assets/logout.png';
-//teacher pngs
+//teacher dashboard pngs
 import progressTracker from '../../assets/TeacherDashboard/progressTracker.png'
 import studentProfiles from '../../assets/TeacherDashboard/studentProfiles.png'
 import helpRequests from '../../assets/TeacherDashboard/helpRequests.png'
 import projectSubmissions from '../../assets/TeacherDashboard/projectSubmissions.png'
 import projectLibrary from '../../assets/TeacherDashboard/projectLibrary.png'
-//student pngs
+//student dashboard pngs
 import learningObjectives from '../../assets/StudentDashboard/learningObjectives.png'
 import instructions from '../../assets/StudentDashboard/instructions.png'
 import video from '../../assets/StudentDashboard/video.png'
@@ -23,7 +23,6 @@ import submitProject from '../../assets/StudentDashboard/submitProject.png'
 //renders student dashboard content 
 function renderStudentDashboardContent(studentName) {
     const studentImagePath = `/images/students/${studentName}.png`;
-
     return (
         <>  
             {/* profile pic */}
@@ -31,6 +30,7 @@ function renderStudentDashboardContent(studentName) {
               <img src={studentImagePath} alt={studentName} className={styles.profilePic}/>
             </div>
 
+            {/* tabs */}
             <div className={styles.tabs}>
               <NavLink to='/student-dashboard/learning-objectives'
               className={({isActive}) => (isActive ? styles.navLinkActive : styles.navLink)}>
@@ -79,13 +79,14 @@ function renderStudentDashboardContent(studentName) {
 //renders teacher dashboard content
 function renderTeacherDashboardContent(teacherName) {
     const teacherImagePath = `/images/teachers/${teacherName}.png`;
-
     return (
         <>  
+            {/* profile pic */}
             <div className={styles.profilePicContainer}>
               <img src={teacherImagePath} alt={teacherName} className={styles.profilePic}/>
             </div>
             
+            {/* tabs */}
             <div className={styles.tabs}>
               <NavLink to='/teacher-dashboard/progress-tracker'
               className={({isActive}) => (isActive ? styles.navLinkActive : styles.navLink)}>
@@ -133,27 +134,29 @@ function renderTeacherDashboardContent(teacherName) {
 
 //main dashboard function
 export default function Dashboard({ user, studentName, teacherName }) {
+
     //defines usestate for dashboard activity
     const [isActive, setIsActive] = useState(false);
     //function for active state toggle
     const toggleDashboard = () => {
         setIsActive(!isActive);
     };
+
     //function that renders dashboard content based on user
     const renderDashboardContent = () => {
+        //if statement based on user
         if (user === 'teacher') {
             return renderTeacherDashboardContent(teacherName);
         } else {
             return renderStudentDashboardContent(studentName);
         }
     };
-    //renders 
     return (
         //main body of page, dashboard + page content
         <main className={styles.mainBody}>
+
           {/* dashboard */}
-          <div className={`${styles.dashboard} ${isActive ? styles.isActive : ''}`}>
-          
+          <div className={`${styles.dashboard} ${isActive ? styles.isActive : ''}`}> 
             {/* tabs */}
             {renderDashboardContent()}
 
@@ -169,9 +172,10 @@ export default function Dashboard({ user, studentName, teacherName }) {
                 <button className={styles.footerBtns}><img src={profile} alt="profile"/><p>Profile</p></button>
                 <button className={styles.footerBtns}><img src={settings} alt="settings" /><p>Settings</p></button>
                 <button className={styles.footerBtns}><img src={logout} alt="logout" /><p>Log Out</p></button>
-            </div>
+            </div>            
           </div>
-          {/* use Outlet to render active tab elements/body */}
+
+          {/* uses Outlet to render active tab elements/body */}
           <div className={styles.outletContent}>
             <Outlet />
           </div>
--- src/pages/TeacherDashboard/TeacherDashboard.jsx
+++ src/pages/TeacherDashboard/TeacherDashboard.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
 import Dashboard from '../../common/Dashboard/Dashboard.jsx';
-import ProgressTracker from './components/ProgressTracker/ProgressTracker.jsx';
-import styles from './TeacherDashboard.module.css';
-import { useState } from 'react';
+import NavbarTwo from '../../common/NavbarTwo.jsx';
 
-export default function TeacherDashboard() {
-  // Assuming you have the teacher's name stored somewhere, 
-  // replace 'teacherNameValue' with the actual name value
-  const teacherName = 'teacherNameValue';
+export default function TeacherDashboard() {  
 
   return (
     <>
         {/* top nav */}
-        <header style={{ outline: '2px solid black' }}>TOP NAV</header>
+        <NavbarTwo user='teacher' />
         
-        {/* dashboard + body */}
+        {/* dashboard + content */}
         <Dashboard user="teacher" teacherName='JasminaSalvador' />
 
         {/* footer */}
