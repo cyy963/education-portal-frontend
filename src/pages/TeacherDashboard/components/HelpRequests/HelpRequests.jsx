241bedf4727e3913626f0f866fa16865ef9f0070
justine padilla <158981238+justinedp@users.noreply.github.com>
2024-04-02 16:14:30 +1300

    updated help request page

--- src/pages/TeacherDashboard/components/HelpRequests/HelpRequests.jsx
+++ src/pages/TeacherDashboard/components/HelpRequests/HelpRequests.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import Checklist from "./checklist/CheckList";
+import scrollStyle from "../../../../common/Scrollbar.module.css"
 
-export default function HelpRequests() {
-  return <div>HelpRequests</div>;
+import styles from "./HelpRequests.module.css";
+import StudentHelp from "./components/StudentHelp";
+
+function HelpRequests() {
+    return (
+      // main body 
+      <div className={styles.body}>
+        <div className={styles.main}>
+
+          {/* supposed to be the scroll bar area */}
+          <div className={styles.forScroll}>
+            <div className={styles.flex}>
+              <h2 className={styles.title}>HELP REQUESTS</h2>
+              <h4 className={styles.otherText}>REPLY</h4>
+              <h4 className={styles.otherText}>MARK AS DONE</h4>
+              
+            </div>
+            <div>
+              {/* <Checklist/ */}
+              <StudentHelp/>
+            </div>
+          </div>
+
+          
+          
+        </div>
+      </div>
+      
+    )
 }
+
+
+export default HelpRequests
