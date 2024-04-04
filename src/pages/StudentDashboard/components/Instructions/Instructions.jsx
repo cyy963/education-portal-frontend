c7dbf7501fee4d390775cd5d82d33ae908a7571e
kyrakMissionReady <158864981+kyrakMissionReady@users.noreply.github.com>
2024-04-04 22:50:21 +1300

    Added 'instructions' jsx and css pages

--- src/pages/StudentDashboard/components/Instructions/Instructions.jsx
+++ src/pages/StudentDashboard/components/Instructions/Instructions.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import styles from './Instructions.module.css';
 
 export default function Instructions() {
-  return <div>Instructions</div>;
+  return (
+    <div className={styles.overlayBox}>
+      <div className={styles.centeredColumn}>
+        <div className={styles.row}>
+          <h2>1. Join Scratch</h2>
+        </div>
+        <div className={styles.row}>
+          <p>If you haven’t used Scratch before, you will need to join Scratch first. 
+            Go to <a href="https://scratch.mit.edu" className={styles.linkStyle}>https://scratch.mit.edu</a>. 
+            Click on <a href="https://scratch.mit.edu/join" 
+            className={styles.linkStyle} target="_blank" rel="noopener noreferrer">Join Scratch</a>.</p>
+        </div>
+        <div className={styles.row}>
+        <img src='/images/projects/Project01-instructions.png' 
+        alt="Create games with scratch" className={styles.imageStyle} />
+        </div>
+        <div className={styles.row}>
+          <p>Follow the instructions to join. You will need a username and a password 
+            that you will remember. If possible, you should also verify your email 
+            address so that you can Share projects later. Ask your teacher to help 
+            with this step if you don’t have an email address, or if you are not sure 
+            what to do.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
