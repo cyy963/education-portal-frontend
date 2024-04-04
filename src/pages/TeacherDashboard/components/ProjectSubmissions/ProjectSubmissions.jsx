bedb7b60b3078ef0eea5037611068a6f5fd0e3dd
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-04-05 11:43:52 +1300

    mark as complete button first milestone. Can delete one line.

--- src/pages/TeacherDashboard/components/ProjectSubmissions/ProjectSubmissions.jsx
+++ src/pages/TeacherDashboard/components/ProjectSubmissions/ProjectSubmissions.jsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
 import styles from "./ProjectSubmissions.module.css";
 import scrollStyle from "../../../../common/Scrollbar.module.css";
 
 // Importing components
 import ProjectSubmissionsButtons from "./components/ProjectSubmissionsButtons";
 import SubmittedProjects from "./components/SubmittedProjects";
+import projectImg from "../../../../assets/StudentDashboard/makeProject-screenshot.png";
 
 export default function ProjectSubmissions() {
+  // Test projects to ensure things work (will replace with fetch eventually)
+  const testProject = [
+    {
+      name: "Aiden",
+      profilePic: "/images/students/AidenAndrews.png",
+      projectPic: { projectImg },
+      date: "TUE 28 April 2020",
+      time: "10:43 AM",
+    },
+    {
+      name: "Rawiri",
+      profilePic: "/images/students/RawiriFletcher.png",
+      date: "TUE 28 April 2020",
+      time: "10:27 AM",
+    },
+    {
+      name: "Neveah",
+      profilePic: "/images/students/NeveahMachenry.png",
+      date: "TUE 28 April 2020",
+      time: "9:58 AM",
+    },
+    {
+      name: "Alice",
+      profilePic: "/images/students/AliceKindellan.png",
+      date: "TUE 28 April 2020",
+      time: "9:50 AM",
+    },
+    {
+      name: "Courtney",
+      profilePic: "/images/students/CourtneyBristol.png",
+      date: "TUE 28 April 2020",
+      time: "9:46 AM",
+    },
+  ];
+
+  const [projects, setProjects] = useState([...testProject]);
+  const [checked, setChecked] = useState([false, false, false, false, false]);
+
   return (
     <div className={styles.content}>
       {/* container for scrollbar content */}
@@ -13,11 +53,25 @@ export default function ProjectSubmissions() {
         <div className={styles.flex}>
           <h3 className={styles.title}>PROJECT SUBMISSIONS</h3>
           <div>
-            <ProjectSubmissionsButtons type="download" />
-            <ProjectSubmissionsButtons type="complete" />
+            <ProjectSubmissionsButtons
+              type="download"
+              projects={projects}
+              checked={checked}
+            />
+            <ProjectSubmissionsButtons
+              type="complete"
+              projects={projects}
+              setProjects={setProjects}
+              checked={checked}
+              setChecked={setChecked}
+            />
           </div>
         </div>
-        <SubmittedProjects />
+        <SubmittedProjects
+          projects={projects}
+          checked={checked}
+          setChecked={setChecked}
+        />
       </div>
     </div>
   );
--- src/pages/TeacherDashboard/components/ProjectSubmissions/components/ProjectSubmissionsButtons.jsx
+++ src/pages/TeacherDashboard/components/ProjectSubmissions/components/ProjectSubmissionsButtons.jsx
@@ -1,17 +1,66 @@
+import { useEffect, useState } from "react";
 // Import styles
 import styles from "../ProjectSubmissions.module.css";
 
-export default function ProjectSubmissionsButtons({ type }) {
+export default function ProjectSubmissionsButtons({
+  type,
+  projects,
+  setProjects,
+  checked,
+  setChecked,
+}) {
+  const [changeIndex, setChangeIndex] = useState();
+
+  //Functions
+  useEffect(() => {
+    let testChangeIndex = [];
+    for (let i = 0; i < checked.length; i++) {
+      if (!checked[i]) {
+        testChangeIndex.push(i);
+      }
+    }
+
+    setChangeIndex(testChangeIndex);
+  }, [checked]);
+
+  function handleCheck() {
+    const changeChecked = projects.map((item) => false);
+    setChecked(changeChecked);
+  }
+
+  const handleDownload = () => {
+    // updateChangeIndex();
+    console.log("Download");
+  };
+
+  const handleMarkComplete = () => {
+    // updateChangeIndex();
+    console.log("Mark complete");
+    console.log(changeIndex);
+
+    // Delete objects from project
+    let updatedProjects;
+
+    updatedProjects = changeIndex.map((item) => {
+      return projects[item];
+    });
+
+    setProjects(updatedProjects);
+
+    // reset checked
+    handleCheck();
+  };
+
   if (type === "download") {
     return (
-      <button className={styles.btn}>
-        <h4 className={styles.btnText}>DOWNLOAD</h4>
+      <button className={styles.btn} onClick={handleDownload}>
+        <h4 className={styles.btnText}> {"\u2B73"} DOWNLOAD FILES</h4>
       </button>
     );
   } else if (type === "complete") {
     return (
-      <button className={styles.btn}>
-        <h4 className={styles.btnText}>MARK AS COMPLETE PROJECT</h4>
+      <button className={styles.btn} onClick={handleMarkComplete}>
+        <h4 className={styles.btnText}>{"\u2713"} MARK AS COMPLETE PROJECT</h4>
       </button>
     );
   } else {
