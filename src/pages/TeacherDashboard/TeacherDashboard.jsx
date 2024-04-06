d2ece068c401c83e222ca5c155fafaedb0711134
cyy963 <cyrusw89@gmail.com>
2024-04-07 00:02:42 +1300

    mac update

--- src/pages/TeacherDashboard/TeacherDashboard.jsx
+++ src/pages/TeacherDashboard/TeacherDashboard.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import styles from './TeacherDashboard.module.css';
 //component imports
 import NavbarTwo from '../../common/NavbarTwo.jsx';
 import Dashboard from '../../common/Dashboard/Dashboard.jsx';
