24c030b9673673de1c1069238c9e0033aa4301c7
cyy963 <cyrusw89@gmail.com>
2024-04-11 21:58:31 +1200

    added scroll bar to progress tracker and student profiles, adjusted nav bar 2 css for button and flags, adjusted borders of the dashboard pages to match

--- src/pages/TeacherDashboard/components/ProgressTracker/ProgressTracker.jsx
+++ src/pages/TeacherDashboard/components/ProgressTracker/ProgressTracker.jsx
@@ -12,104 +12,154 @@ export default function ProgressTracker() {
             </div>
 
             {/* studentBarContainer */}
-            <div className={styles.studentBarContainer}>
+            <div className={styles.forScroll}>
+              <div className={styles.studentBarContainer}>
 
-              <div className={styles.studentProgressBar}>
-                <div className={styles.studentInfo}>
-                  <p className={styles.studentName}>AIDEN ANDREWS</p>
-                  <p className={styles.studentProgress}>4/15 Projects Completed</p>
+                <div className={styles.studentProgressBar}>
+                  <div className={styles.studentInfo}>
+                    <p className={styles.studentName}>AIDEN ANDREWS</p>
+                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
+                  </div>
+                  <div className={styles.progressIconContainer}>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>10</button>
+                    <button>11</button>
+                    <button>12</button>
+                    <button>13</button>
+                    <button>14</button>
+                    <button>15</button>
+                  </div>
                 </div>
-                <div className={styles.progressIconContainer}>
-                  <button>1</button>
-                  <button>2</button>
-                  <button>3</button>
-                  <button>4</button>
-                  <button>5</button>
-                  <button>6</button>
-                  <button>7</button>
-                  <button>8</button>
-                  <button>9</button>
-                  <button>10</button>
-                  <button>11</button>
-                  <button>12</button>
-                  <button>13</button>
-                  <button>14</button>
-                  <button>15</button>
+
+                <div className={styles.studentProgressBar}>
+                  <div className={styles.studentInfo}>
+                    <p className={styles.studentName}>AIDEN ANDREWS</p>
+                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
+                  </div>
+                  <div className={styles.progressIconContainer}>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>10</button>
+                    <button>11</button>
+                    <button>12</button>
+                    <button>13</button>
+                    <button>14</button>
+                    <button>15</button>
+                  </div>
                 </div>
+
+                <div className={styles.studentProgressBar}>
+                  <div className={styles.studentInfo}>
+                    <p className={styles.studentName}>AIDEN ANDREWS</p>
+                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
+                  </div>
+                  <div className={styles.progressIconContainer}>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>10</button>
+                    <button>11</button>
+                    <button>12</button>
+                    <button>13</button>
+                    <button>14</button>
+                    <button>15</button>
+                  </div>
+                </div>
+
+                <div className={styles.studentProgressBar}>
+                  <div className={styles.studentInfo}>
+                    <p className={styles.studentName}>AIDEN ANDREWS</p>
+                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
+                  </div>
+                  <div className={styles.progressIconContainer}>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>10</button>
+                    <button>11</button>
+                    <button>12</button>
+                    <button>13</button>
+                    <button>14</button>
+                    <button>15</button>
+                  </div>
+                </div>
+
+                <div className={styles.studentProgressBar}>
+                  <div className={styles.studentInfo}>
+                    <p className={styles.studentName}>AIDEN ANDREWS</p>
+                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
+                  </div>
+                  <div className={styles.progressIconContainer}>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>10</button>
+                    <button>11</button>
+                    <button>12</button>
+                    <button>13</button>
+                    <button>14</button>
+                    <button>15</button>
+                  </div>
+                </div>
+                
+                <div className={styles.studentProgressBar}>
+                  <div className={styles.studentInfo}>
+                    <p className={styles.studentName}>AIDEN ANDREWS</p>
+                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
+                  </div>
+                  <div className={styles.progressIconContainer}>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>10</button>
+                    <button>11</button>
+                    <button>12</button>
+                    <button>13</button>
+                    <button>14</button>
+                    <button>15</button>
+                  </div>
+                </div>
+
               </div>
-
-              <div className={styles.studentProgressBar}>
-                <div className={styles.studentInfo}>
-                  <p className={styles.studentName}>AIDEN ANDREWS</p>
-                  <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                </div>
-                <div className={styles.progressIconContainer}>
-                  <button>1</button>
-                  <button>2</button>
-                  <button>3</button>
-                  <button>4</button>
-                  <button>5</button>
-                  <button>6</button>
-                  <button>7</button>
-                  <button>8</button>
-                  <button>9</button>
-                  <button>10</button>
-                  <button>11</button>
-                  <button>12</button>
-                  <button>13</button>
-                  <button>14</button>
-                  <button>15</button>
-                </div>
-              </div>
-
-              <div className={styles.studentProgressBar}>
-                <div className={styles.studentInfo}>
-                  <p className={styles.studentName}>AIDEN ANDREWS</p>
-                  <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                </div>
-                <div className={styles.progressIconContainer}>
-                  <button>1</button>
-                  <button>2</button>
-                  <button>3</button>
-                  <button>4</button>
-                  <button>5</button>
-                  <button>6</button>
-                  <button>7</button>
-                  <button>8</button>
-                  <button>9</button>
-                  <button>10</button>
-                  <button>11</button>
-                  <button>12</button>
-                  <button>13</button>
-                  <button>14</button>
-                  <button>15</button>
-                </div>
-              </div>
-
-              <div className={styles.studentProgressBar}>
-                <div className={styles.studentInfo}>
-                  <p className={styles.studentName}>AIDEN ANDREWS</p>
-                  <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                </div>
-                <div className={styles.progressIconContainer}>
-                  <button>1</button>
-                  <button>2</button>
-                  <button>3</button>
-                  <button>4</button>
-                  <button>5</button>
-                  <button>6</button>
-                  <button>7</button>
-                  <button>8</button>
-                  <button>9</button>
-                  <button>10</button>
-                  <button>11</button>
-                  <button>12</button>
-                  <button>13</button>
-                  <button>14</button>
-                  <button>15</button>
-                </div>
-              </div>
-
             </div>
         </div>
       
