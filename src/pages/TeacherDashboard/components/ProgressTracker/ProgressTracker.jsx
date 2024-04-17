be240e45f7fbc076e48bb2888628641bf075ab8e
cyy963 <cyrusw89@gmail.com>
2024-04-18 00:54:19 +1200

    added fetch for progress-tracker, updated progress bar to use .map for name and icons, adjusted its css accordingly to use vw/vh instead of px

--- src/pages/TeacherDashboard/components/ProgressTracker/ProgressTracker.jsx
+++ src/pages/TeacherDashboard/components/ProgressTracker/ProgressTracker.jsx
@@ -1,168 +1,50 @@
 import styles from './ProgressTracker.module.css';
+import { useEffect, useState } from 'react';
 
 export default function ProgressTracker() {
+  const [students, setStudents] = useState([]);
+
+  useEffect (()=>{
+    fetch('http://localhost:4000/teacher-dashboard/progress-tracker')
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result)
+        setStudents(result);
+      });
+  }, []);
+
   return (
-    
-      
-        <div className={styles.body}>
-            {/* top bar */}
-            <div className={styles.title}>
-              <h2>BEGINNER COURSE</h2>
-              <h5>EXPORT AS SPREADSHEET</h5>
-            </div>
-
-            {/* studentBarContainer */}
-            <div className={styles.forScroll}>
-              <div className={styles.studentBarContainer}>
-
-                <div className={styles.studentProgressBar}>
-                  <div className={styles.studentInfo}>
-                    <p className={styles.studentName}>AIDEN ANDREWS</p>
-                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                  </div>
-                  <div className={styles.progressIconContainer}>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>10</button>
-                    <button>11</button>
-                    <button>12</button>
-                    <button>13</button>
-                    <button>14</button>
-                    <button>15</button>
-                  </div>
-                </div>
-
-                <div className={styles.studentProgressBar}>
-                  <div className={styles.studentInfo}>
-                    <p className={styles.studentName}>AIDEN ANDREWS</p>
-                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                  </div>
-                  <div className={styles.progressIconContainer}>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>10</button>
-                    <button>11</button>
-                    <button>12</button>
-                    <button>13</button>
-                    <button>14</button>
-                    <button>15</button>
-                  </div>
-                </div>
-
-                <div className={styles.studentProgressBar}>
-                  <div className={styles.studentInfo}>
-                    <p className={styles.studentName}>AIDEN ANDREWS</p>
-                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                  </div>
-                  <div className={styles.progressIconContainer}>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>10</button>
-                    <button>11</button>
-                    <button>12</button>
-                    <button>13</button>
-                    <button>14</button>
-                    <button>15</button>
-                  </div>
-                </div>
-
-                <div className={styles.studentProgressBar}>
-                  <div className={styles.studentInfo}>
-                    <p className={styles.studentName}>AIDEN ANDREWS</p>
-                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                  </div>
-                  <div className={styles.progressIconContainer}>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>10</button>
-                    <button>11</button>
-                    <button>12</button>
-                    <button>13</button>
-                    <button>14</button>
-                    <button>15</button>
-                  </div>
-                </div>
-
-                <div className={styles.studentProgressBar}>
-                  <div className={styles.studentInfo}>
-                    <p className={styles.studentName}>AIDEN ANDREWS</p>
-                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                  </div>
-                  <div className={styles.progressIconContainer}>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>10</button>
-                    <button>11</button>
-                    <button>12</button>
-                    <button>13</button>
-                    <button>14</button>
-                    <button>15</button>
-                  </div>
-                </div>
-                
-                <div className={styles.studentProgressBar}>
-                  <div className={styles.studentInfo}>
-                    <p className={styles.studentName}>AIDEN ANDREWS</p>
-                    <p className={styles.studentProgress}>4/15 Projects Completed</p>
-                  </div>
-                  <div className={styles.progressIconContainer}>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>10</button>
-                    <button>11</button>
-                    <button>12</button>
-                    <button>13</button>
-                    <button>14</button>
-                    <button>15</button>
-                  </div>
-                </div>
-
-              </div>
-            </div>
+    <div className={styles.body}>
+        {/* top bar */}
+        <div className={styles.title}>
+          <h2>BEGINNER COURSE</h2>
+          <h5>EXPORT AS SPREADSHEET</h5>
         </div>
-      
-    
+
+        {/* studentBarContainer */}
+        <div className={styles.forScroll}>
+          <div className={styles.studentBarContainer}>
+            {students.map((student, studentId) => {
+              return (
+                <div className={styles.studentProgressBar}>
+                  <div className={styles.studentInfo}>
+                    <p className={styles.studentName}>{student.name.toUpperCase()}</p>
+                    <p className={styles.studentProjectsCompleted}>4/15 Projects Completed</p>
+                  </div>
+                  <div className={styles.progressIconContainer}>
+                    {students.map((student, studentId) => {
+                      return (
+                        <div className={styles.progressIcons} key={studentId}>
+                          <p className={styles.studentId}>{student.student_id}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+    </div>
   )
 }
