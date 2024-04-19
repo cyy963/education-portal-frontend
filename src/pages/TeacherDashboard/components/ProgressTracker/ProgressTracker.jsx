664b96949069d995cc45769d9a9763b5b25201c5
cyy963 <cyrusw89@gmail.com>
2024-04-19 12:38:45 +1200

    added export spreadsheet function to student progress along with neccessary imports and packages. added error catching to both pages fetch. added comments to both pages. added useCallback hook, and stored function results in const to optimize code

--- src/pages/TeacherDashboard/components/ProgressTracker/ProgressTracker.jsx
+++ src/pages/TeacherDashboard/components/ProgressTracker/ProgressTracker.jsx
@@ -1,76 +1,111 @@
 import styles from './ProgressTracker.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import { CSVLink } from 'react-csv';
+import fileExport from '../../../../assets/TeacherDashboard/fileExport.svg'
 
 export default function ProgressTracker() {
   const [projects, setProjects] = useState([]);
   const [projectResults, setProjectResults] = useState([]);
+  const [exportData, setExportData] = useState([]);
   
-  //fetch project_id from project table
+  //fetches and sets project_id from project table into projects
   useEffect (()=>{
     fetch('http://localhost:4000/projects')
       .then((response) => response.json())
       .then((result) => {
         console.log(result)
         setProjects(result);
+      })
+      //error catching
+      .catch((error) => {
+        console.error(error);
       });
   }, []);
-  //fetch project_results excel
+
+  //fetches and sets students and their corresponding 
+  //number of and string of compeleted projects into projectResults
   useEffect (()=>{
     fetch('http://localhost:4000/project_results')
       .then((response) => response.json())
       .then((result) => {
         console.log(result)
         setProjectResults(result);
+      })
+      //error catching
+      .catch((error) => {
+        console.error(error);
       });
   }, []);
 
-  //function returns true if projectId is in completed_projects
-  function checkCompletedProjects(completedProjects, projectId) {
+  //sets student progress data into setExportData
+  useEffect(() => {
+    const data = projectResults.map((projectResult) => {
+      return {
+        StudentID: projectResult.student_id,
+        StudentName: projectResult.name,
+        CompletedProjects: projectResult.completed_projects,
+        TotalCompletedProjects: projectResult.total_completed_projects,
+      };
+    });
+    setExportData(data);
+  }, [projectResults]);
+
+  //function returns true if projectId is in completedProjects
+  //useCallBack hook to memorize/caches the function, only recalculates it when dependencies change
+  const checkCompletedProjects = useCallback((completedProjects, projectId) => {
     return completedProjects && completedProjects.includes(projectId);
-  };
+    //no dependencies so only recalculated when component is mounted or unmounted
+  }, []);
 
   return (
     <div className={styles.body}>
         {/* top bar */}
         <div className={styles.title}>
           <h2>BEGINNER COURSE</h2>
-          <h5>EXPORT AS SPREADSHEET</h5>
+          <div className={styles.exportContainer}>           
+            <CSVLink data={exportData} filename="student-progress.csv" className="export-spreadsheet">               
+              <img className={styles.exportIcon} src={fileExport} alt='exportIcon'/>
+              <h5>EXPORT AS SPREADSHEET</h5>                
+            </CSVLink>           
+          </div>
         </div>
 
         {/* students progress tracker container */}
         <div className={styles.forScroll}>
           <div className={styles.studentBarContainer}>
-            {projectResults.map((projectResult, studentId) => {
-              return (
-                //student progress bar
-                <div className={styles.studentProgressBar} key={studentId}>
-                  {/* student name and projects completed fraction */}
-                  <div className={styles.studentInfo}>
-                    <p className={styles.studentName}>{projectResult.name.toUpperCase()}</p>
-                    <p className={styles.studentProjectsCompletedFraction}>{projectResult.complete_projects_number}/15 Projects Completed</p>
+            {/* only .maps projectResults once */}
+            {projectResults.map((projectResult) => { 
+                return (
+                  //student progress bar
+                  <div className={styles.studentProgressBar} key={projectResult.student_id}>
+                    {/* student name and projects completed fraction */}
+                    <div className={styles.studentInfo}>
+                      <p className={styles.studentName}>{projectResult.name.toUpperCase()}</p>
+                      <p className={styles.studentProjectsCompletedFraction}>{projectResult.total_completed_projects}/15 Projects Completed</p>
+                    </div>
+                    {/* progress icons container */}
+                    <div className={styles.progressIconContainer}>
+                      {projects.map((project) => {
+                        //stores checkCompletedProjects as a const
+                        const completed = checkCompletedProjects(projectResult.completed_projects, project.project_id);
+                        return (
+                          //progress icon
+                          <div
+                            //changes icon style to green if it is complete
+                            className={`${styles.progressIcons} ${completed ? styles.completeProgressIcons : styles.incompleteProgressIcons}`}
+                            //uses student_id as unique key
+                            key={project.project_id}
+                          >
+                            {project.project_id}
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
-                  {/* progress icons container */}
-                  <div className={styles.progressIconContainer}>
-                    {projects.map((project, projectId) => {
-                      return (
-                        //progress icon
-                        <div
-                        //changes icon style based on checkCompeletedProjects function
-                        className={`${styles.progressIcons} 
-                        ${checkCompletedProjects(projectResult.completed_projects, project.project_id) ? `${styles.completeProgressIcons}` : "" }`} 
-                        key={projectId}>
-                          {project.project_id}
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
+                );
             })}
           </div>
         </div>
     </div>
   )
 }
-
-// ${checkCompletedProjects(project.project_id, projectResult.completed_projects) ? `${styles.completeProgressIcons}` : "" }
--- src/pages/TeacherDashboard/components/StudentProfiles/StudentProfiles.jsx
+++ src/pages/TeacherDashboard/components/StudentProfiles/StudentProfiles.jsx
@@ -4,12 +4,17 @@ import { useEffect, useState } from 'react';
 export default function StudentProfiles(){
   const [students, setStudents] = useState([]);
 
+  //fetches and sets student name and id to students
   useEffect (()=>{
     fetch('http://localhost:4000/students')
       .then((response) => response.json())
       .then((result) => {
         console.log(result)
         setStudents(result);
+      })
+      //error catching
+      .catch((error) => {
+        console.error(error);
       });
   }, []);
 
@@ -18,10 +23,13 @@ export default function StudentProfiles(){
         <div className={styles.forScroll}>
           {/* student profiles container */}
           <div className={styles.studentsProfileContainer}>
-            {students.map((student, index) => {
+            {students.map((student) => {
               return (
-                <div className={styles.studentCard} key={index}>
+                //student card
+                <div className={styles.studentCard} key={student.student_id}>
+                  {/* student profile pic */}
                   <img src={student.profile_pic} className={styles.studentImg}/>
+                  {/* student name */}
                   <p className={styles.studentName}>{student.name.toUpperCase()}</p>
                 </div>
               );
