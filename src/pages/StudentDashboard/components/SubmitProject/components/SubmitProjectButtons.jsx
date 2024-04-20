cf43d78cba6db5d5a5396134a86dce6ce8fb6deb
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-04-20 13:19:11 +1200

    Added filestack and dynamic submission.

--- src/pages/StudentDashboard/components/SubmitProject/components/SubmitProjectButtons.jsx
+++ src/pages/StudentDashboard/components/SubmitProject/components/SubmitProjectButtons.jsx
@@ -1,3 +1,10 @@
+// Filestack
+import React, { useEffect, useState } from "react";
+import { PickerOverlay } from "filestack-react-18";
+
+// React router dom
+import { useParams } from "react-router-dom";
+
 // Import images
 import submitIcon from "../../../../../assets/StudentDashboard/sendPhoto.png";
 import callIcon from "../../../../../assets/StudentDashboard/callTeacher.png";
@@ -6,13 +13,56 @@ import callIcon from "../../../../../assets/StudentDashboard/callTeacher.png";
 import styles from "../SubmitProject.module.css";
 
 export default function SubmitProjectButtons({ type }) {
+  const [isPickerOverlayVisible, setIsPickerOverlayVisible] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+  const params = useParams();
+
+  const handleVisible = () => {
+    setIsPickerOverlayVisible(!isPickerOverlayVisible);
+  };
+
+  console.log(params.projectId);
+
+  useEffect(() => {
+    const date = new Date().toISOString().slice(0, 19).replace("T", " ");
+    console.log(date);
+    imageUrl &&
+      fetch("http://localhost:4000/api/submit-project", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          studentId: 12,
+          projectId: params.projectId,
+          dateComp: date,
+          img: imageUrl,
+        }),
+      }).then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          console.log("submit successful");
+        }
+      });
+  }, [imageUrl]);
+
   if (type === "photo") {
     return (
       <div>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={handleVisible}>
           <img src={submitIcon} alt="submit photo icon" />
           <p className={styles.btnText}>Send photo</p>
         </button>
+
+        <div>
+          {isPickerOverlayVisible && (
+            <PickerOverlay
+              apikey="AbdjiOOBuSECXjQRpcVtez"
+              onSuccess={(result) => {
+                setImageUrl(result.filesUploaded[0].url);
+                setIsPickerOverlayVisible(false);
+              }}
+            />
+          )}
+        </div>
       </div>
     );
   } else if (type === "call") {
