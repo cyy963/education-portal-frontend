65a0991f219c7a751d30bd45a7ad8205d1ccc44e
justine padilla <158981238+justinedp@users.noreply.github.com>
2024-04-24 20:49:22 +1200

    fixing merging error

--- src/pages/TeacherDashboard/components/HelpRequests/components/HelpRequestButtons.jsx
+++ src/pages/TeacherDashboard/components/HelpRequests/components/HelpRequestButtons.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import styles from "../HelpRequests.module.css"
-
+ 
 function HelpRequestButtons({ requests, checked, type, setRequests, setChecked }) {
     const [changeIndex, setChangeIndex] = useState([]);
     const [changed, setChanged] = useState([]);
-
+ 
     useEffect(() => {
         let testChangeIndex = [];
         let testChanged = [];
@@ -17,11 +17,34 @@ function HelpRequestButtons({ requests, checked, type, setRequests, setChecked }
             testChanged.push(checked[i]);
           }
         }
-     
         setChangeIndex(testChangeIndex);
         setChanged(testChanged);
       }, [checked]);
-
+ 
+    const markAsDone = () => {
+        requests.map((user, index) => {
+            if (
+                user.student_id === checked[index].studentId &&
+                user.request_id === checked[index].requestId &&
+                checked[index].checked === true
+            ) {
+                fetch("http://localhost:4000/help-requests", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        studentId: user.student_id,
+                        requestId: user.request_id,
+                        done: 1,
+                      })
+                }).then((res) => {
+                    console.log(res);
+                    if (res.status === 200) {
+                        console.log("marked done")
+                    }
+                })
+            }
+        })
+ 
       const markAsDone = () => {
         requests.map((user, index) => {
           if (
@@ -47,19 +70,19 @@ function HelpRequestButtons({ requests, checked, type, setRequests, setChecked }
             });
           }
         });
-      
+     
         let updatedRequests;
-      
+     
         updatedRequests = changeIndex.map((item) => {
           return requests[item];
         });
-      
+     
         setRequests(updatedRequests);
-      
+     
         const updatedChecked = changed.map((item) => {
           return { studentId: item.studentId, requestId: item.requestId, checked: false };
         });
-      
+     
         setChecked(updatedChecked);
       };
        
@@ -73,5 +96,6 @@ function HelpRequestButtons({ requests, checked, type, setRequests, setChecked }
         return <p>Something went wrong</p>;
     }
 }
-    
+}
+   
 export default HelpRequestButtons
